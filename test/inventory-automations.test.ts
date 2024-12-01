15f03811f0f5ae2295ba46dcd80008dee69a1d20
Chris Enchev <Foriee007@users.noreply.github.com>
2024-12-01 04:22:15 -0800

    Add mock test - DynamoDB

--- test/inventory-automations.test.ts
+++ test/inventory-automations.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as InventoryAutomations from '../lib/inventory-automations-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import {InventoryAutomationsStack} from "../lib/inventory-automations-stack";
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/inventory-automations-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new InventoryAutomations.InventoryAutomationsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('DynamoDB Table Created', () => {
+    const app = new cdk.App();
+    const stack = new InventoryAutomationsStack(app, 'TestStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const template = Template.fromStack(stack);
+
+    // Assert DynamoDB Table exists
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        BillingMode: 'PAY_PER_REQUEST',
+    });
 });
